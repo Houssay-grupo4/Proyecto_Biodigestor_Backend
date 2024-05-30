99cc20af7167769dfeddcd4eab23ea3c792d9292
Paz-Santangelo <paz_santangelo@hotmail.com>
2024-05-30 14:53:00 -0300

    Subiendo proyecto de base para comenzar a trabajar

---
+++ Proyecto.Biodigestor/Models/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Proyecto.Biodigestor.Models
+{
+    public class Cliente
+    {
+        [Key]
+        [Column("IdCliente")]
+        public int IdCliente { get; set; }
+
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")]
+        public string Nombre { get; set; } = null!;
+
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")]
+        public string Apellido { get; set; } = null!;
+    }
+}
---
+++ Proyecto.Biodigestor/Models/ClientesContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Proyecto.Biodigestor.Models
+{
+    public class ClientesContext : DbContext
+    {
+        public ClientesContext(DbContextOptions<ClientesContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Cliente>().HasIndex(cliente => cliente.IdCliente).IsUnique();
+        }
+    }
+}
