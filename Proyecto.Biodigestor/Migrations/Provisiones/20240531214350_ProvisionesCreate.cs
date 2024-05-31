2e58b6b542cef628caae8e62c7b5c2ffe7bb85b3
Alejandro-Santangelo <artesaniaslarustica@gmail.com>
2024-05-31 20:15:13 -0300

    Provisiones

---
+++ Proyecto.Biodigestor/Migrations/Provisiones/20240531214350_ProvisionesCreate.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Proyecto.Biodigestor.Migrations.Provisiones
+{
+    /// <inheritdoc />
+    public partial class ProvisionesCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cliente",
+                columns: table => new
+                {
+                    IdCliente = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Apellido = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cliente", x => x.IdCliente);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Provisiones",
+                columns: table => new
+                {
+                    IdProvision = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Fecha = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IdCliente = table.Column<int>(type: "int", nullable: false),
+                    CantidadGas = table.Column<int>(type: "int", nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Provisiones", x => x.IdProvision);
+                    table.ForeignKey(
+                        name: "FK_Provisiones_Cliente_IdCliente",
+                        column: x => x.IdCliente,
+                        principalTable: "Cliente",
+                        principalColumn: "IdCliente",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Provisiones_IdCliente",
+                table: "Provisiones",
+                column: "IdCliente");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Provisiones_IdProvision",
+                table: "Provisiones",
+                column: "IdProvision",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Provisiones");
+
+            migrationBuilder.DropTable(
+                name: "Cliente");
+        }
+    }
+}
---
+++ Proyecto.Biodigestor/Models/ProvisionesContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Proyecto.Biodigestor.Models
+{
+    public class ProvisionesContext : DbContext
+    {
+        public ProvisionesContext(DbContextOptions<ProvisionesContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Provision> Provisiones { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Provision>().HasIndex(provision => provision.IdProvision).IsUnique();
+        }
+    }
+}
